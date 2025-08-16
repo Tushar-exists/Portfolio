6535120dceb2c71a4e1923568dec4297b88062e2
Tushar <tushar.kr.bh@gmail.com>
2025-08-16 15:19:36 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import IntroSection from "@/components/sections/intro-section";
-import AboutSection from "@/components/sections/about-section";
-import SkillsSection from "@/components/sections/skills-section";
-import ProjectsSection from "@/components/sections/projects-section";
-import ExperienceSection from "@/components/sections/experience-section";
-import AchievementsSection from "@/components/sections/achievements-section";
-import ContactSection from "@/components/sections/contact-section";
+import IntroSection from "../components/sections/intro-section";
+import AboutSection from "../components/sections/about-section";
+import SkillsSection from "../components/sections/skills-section";
+import ProjectsSection from "../components/sections/projects-section";
+import ExperienceSection from "../components/sections/experience-section";
+import AchievementsSection from "../components/sections/achievements-section";
+import ContactSection from "../components/sections/contact-section";
 
 export default function Home() {
   return (
